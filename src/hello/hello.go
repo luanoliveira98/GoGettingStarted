520d08795ef095e251064f0b065626adab824a2c
Luan Oliveira <luanoliveira17@hotmail.com>
2020-09-22 11:49:25 -0300

    Arrays in Go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -59,6 +59,12 @@ func getCommand() int {
 func startMoniroting() {
 
 	fmt.Println("Monitoring...")
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com/"
+	sites[1] = "https://www.alura.com.br"
+	sites[2] = "https://www.caelum.com.br"
+
+	fmt.Println(sites)
 
 	site := "https://random-status-code.herokuapp.com/"
 	resp, _ := http.Get(site)
